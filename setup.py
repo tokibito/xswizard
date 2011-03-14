43e77fabcdfdf07217114a0b6a8dd6c335692000
Shinya Okano <tokibito@gmail.com>
2011-03-14 23:53:11 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+ 
+setup (
+    name='xswizard',
+    version='0.1',
+    description='XenServer API wrapper',
+    author='Shinya Okano',
+    author_email='tokibito@gmail.com',
+    url='https://bitbucket.org/tokibito/xswizard/',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Plugins',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Programming Language :: Python',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['xswizard'],
+    #test_suite='tests',
+)
