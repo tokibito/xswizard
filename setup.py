88ef12f1d892b5e8cce5ecf52801f19639e6df1a
Shinya Okano <tokibito@gmail.com>
2011-07-29 06:46:45 +0900

    update setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xswizard',
-    version='0.1',
+    version='0.2',
     description='XenServer API wrapper',
     author='Shinya Okano',
     author_email='tokibito@gmail.com',
